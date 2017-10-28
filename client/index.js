ea99d8105a0d3704a7e9f3467655dfc8a76c1a8b
Barto Molina <bartomolina@gmail.com>
2017-10-27 20:31:53 -0400

    added datepicker

---
+++ client/index.js
@@ -0,0 +1,48 @@
+// var Chart = require('react-d3-core').Chart;
+// var LineChart = require('react-d3-basic').LineChart;
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main from './Main'
+
+ReactDOM.render(<Main />, document.getElementById('root'))
+
+// (function () {
+//     // load your general data
+//     var chartData = require('../data/chartData.json');
+
+//     var width = 700,
+//         height = 300,
+//         margins = { left: 100, right: 100, top: 50, bottom: 50 },
+//         title = "User sample",
+//         // chart series,
+//         // field: is what field your data want to be selected
+//         // name: the name of the field that display in legend
+//         // color: what color is the line
+//         chartSeries = [
+//             {
+//                 field: 'BMI',
+//                 name: 'BMI',
+//                 color: '#ff7f0e'
+//             }
+//         ],
+//         // your x accessor
+//         x = function (d) {
+//             return d.index;
+//         }
+
+//     ReactDOM.render(
+
+//         <LineChart
+//             showXGrid={false}
+//             showYGrid={false}
+//             margins={margins}
+//             title={title}
+//             data={chartData}
+//             width={width}
+//             height={height}
+//             chartSeries={chartSeries}
+//             x={x}
+//         />
+//         , document.getElementById('chart')
+//     )
+// })()
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const express = require('express')
+const app = express()
+const path = require('path')
 
+app.use('/dist', express.static(path.join(__dirname, 'dist')))
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')))
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
-
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
